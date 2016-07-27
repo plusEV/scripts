22eedd8a8f6021eef76232de055f15be1a7afffe
raddy <radikal@gmail.com>
2016-07-27 23:40:20 +0530

    metadata fix

--- generate_shfe_h5s.py
+++ generate_shfe_h5s.py
@@ -6,26 +6,29 @@ import sys,os
 import numpy as np
 from kit_time import fix_timestamps
 
-def sim_command(exec_path='/home/jgreenwald/tyche/build/sim/md_dump', config_path= '/home/jgreenwald/configs/md_config_KOSPI'):
+
+def sim_command(exec_path='/home/jgreenwald/tyche/build/sim/md_dump',
+                config_path= '/home/jgreenwald/configs/md_config_SHFE'):
     return  exec_path + ' ' + config_path + ' 0'
 
-def main(mypath = "/home/Dumps2/ProcessedData/bs/", cmd = sim_command(),\
-         cfg_path = '/home/jgreenwald/configs/MDConfig_KOSPI.cfg'):
+def main(mypath = "/home/Dumps2/ProcessedData/", cmd = sim_command(),\
+         cfg_path = '/home/jgreenwald/configs/MDConfig_SHFE.cfg'):
     
     dates = listdir(mypath)
-    md_root = "/home/jgreenwald/md/korea/"
+    dd = pd.Series(dates)
+    dates = dd.ix[dd.str[0]=="2"]
+    md_root = "/home/jgreenwald/md/CHINA_SHFE/"
 
     s = pd.Series([f.strip() for f in listdir(md_root)])
     if s.empty:
         h5s = pd.Series([])
     else:
         h5s = s.ix[s.str[-2:]=='h5']
-
-    last_possible_date = (pd.to_datetime('today') + np.timedelta64(1,'D')).strftime("%Y%m%d")
     #for d in dates:
     for d in dates:
         try:
-            if (d+'.h5') in h5s.values or d<"20160401" or d>last_possible_date:
+            metadata_file = mypath + d + "/CHINA_SHFE_METADATA.dat"
+            if (d+'.h5') in h5s.values or d < "20160501" or not os.path.isfile(metadata_file):
                 continue
             update_cfg(cfg_path,d)
             p = Popen(cmd, stderr=PIPE, stdout=PIPE, shell=True)
@@ -36,10 +39,10 @@ def main(mypath = "/home/Dumps2/ProcessedData/bs/", cmd = sim_command(),\
                 print output
                 print errors
             md = pd.read_csv(md_root+d+'.csv',sep=' ')
-            md.local_time = pd.to_datetime(fix_timestamps(md.local_time.values)).tz_localize("UTC").tz_convert("Asia/Seoul")
+            md.local_time = pd.to_datetime(fix_timestamps(md.local_time.values)).tz_localize("UTC").tz_convert("Asia/Shanghai")
             backup = md.exchange_time
             try:
-                md.exchange_time = pd.Index(pd.to_datetime(md.exchange_time*1e6)).tz_localize("UTC").tz_convert("Asia/Seoul")
+                md.exchange_time = pd.Index(pd.to_datetime(md.exchange_time*1e6)).tz_localize("UTC").tz_convert("Asia/Shanghai")
             except:
                 md.exchange_time = backup
             md.index = md.local_time
@@ -52,8 +55,6 @@ def main(mypath = "/home/Dumps2/ProcessedData/bs/", cmd = sim_command(),\
             print 'Failed to parse: ' + d
             continue
 
-
-
 def update_cfg(path,d):
     assert(len(d) == 8)
     with open(path,'r') as f:
@@ -62,20 +63,7 @@ def update_cfg(path,d):
     p3 = s.find('2', p1)
     ss =  s[:p3] + d + s[p3+8:]
     
-    p1 = ss.find('EXCHID = ')
-    p3 = ss.find('\n',p1)
-    
-    v1 = 'EXCHID = KOSPI_FUT, KOSPI_CALL, KOSPI_PUT'
-    v2 = 'EXCHID = KOSPISHM'
-    
-    
-    sss = ss[:p1] + v1 + ss[p3:]
-    if d > '20160624':
-        sss = ss[:p1] + v2 + ss[p3:]
     with open(path,'w') as f:
-        f.write(sss)
-
+        f.write(ss)
 if __name__ == "__main__":
     sys.exit(main())
-
-
